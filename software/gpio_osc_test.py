2d7236e861ef77148fae8b5b196b5f98842150a5
carlesp-onielfa <98perezcarles@gmail.com>
2020-03-19 11:42:56 +0100

    Multiple synths and preset change

--- software/gpio_osc_test.py
+++ software/gpio_osc_test.py
@@ -5,50 +5,75 @@ import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
 
-button_pins = [37,35,33,31,29,15]
+class Synth:
+    def __init__(self, client, synthn):
+        #Client for osc communication with supercollider
+        self.client = client
+        self.synthn = synthn
+        self.synth_address = '/'+str(synthn)
+        self.preset = 'organ'
+
+    def osc_preset(self,new_preset):
+        self.message("Preset from {} to {}".format(self.preset, new_preset))
+        self.preset = new_preset
+        self.client.send_message(self.synth_address+'/presetChange',new_preset)
+    def osc_pitch(self,input, octave_range = 4):
+        #From 0 - 1024 to -octave_range - octave_range
+        corrected_input = ((input-512)/512.0)*octave_range
+        self.client.send_message(self.synth_address+'/pitchBend',corrected_input)
+    def osc_amp(self,input):
+        #From 0-1024 to 0-1
+        corrected_input = input/1024.0
+        self.client.send_message(self.synth_address+'/ampChange', corrected_input)
+    def osc_note(self,input, note_state):
+        #Button wasn't pressed before then note on
+        if(not note_state[input]):
+            self.message("Note on: {}".format(input))
+            self.client.send_message(self.synth_address+'/noteOn',input)
+            note_state[input] = True
+            time.sleep(0.2)
+        #else note off
+        else:
+            self.message("Note off: {}".format(input))
+            self.client.send_message(self.synth_address+'/noteOff',input)
+            note_state[input] = False
+    def message(self, s):
+        print("LOG | SYNTH {}: ".format(self.synthn)+s)
+#General parameters
+max_synths = 2
+active_synth = 1
+
+#OSC client
+client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
+
+#Synths array
+synths = [Synth(client,i) for i in range(max_synths)]
+
+#Aux button
+aux_pin = 29
+#Keyboard buttons pins in order starting from C4 (60)
+button_pins = [37,35,33,31]
+#Indicates if note is on or off
 note_state = {i+60:False for i in range(len(button_pins))}
 
-def osc_pitch(client, input, synth_n = 0, octave_range = 4):
-    client.send_message('/pitchBend',((input-512)/512.0)*octave_range)
-def osc_amp(client, input, synth_n = 0):
-    client.send_message('/ampChange',input/1024.0)
-def osc_note(client, input, synth_n=0):
-    #Button wasn't pressed before
-    if(not note_state[input]):
-        print("Note on: {}".format(input))
-        client.send_message('/noteOn',input)
-        note_state[input] = True
-        time.sleep(0.2)
-    #Falling edge
-    else:
-        print("Note off: {}".format(input))
-        client.send_message('/noteOff',input)
-        note_state[input] = False
-
-# Hardware SPI configuration:
+#GPIO setup
+GPIO.setmode(GPIO.BOARD)
+# Hardware SPI configuration for dials:
 SPI_PORT   = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
-#pin list, first is notenumber 60
-
-
-GPIO.setmode(GPIO.BOARD)
+#Setup keyboard buttons pins and interrupts
 for i,pin in enumerate(button_pins):
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(pin, GPIO.BOTH, callback = lambda f : osc_note(client, i+60))
+    GPIO.add_event_detect(pin, GPIO.BOTH, callback = lambda pin,i=i: synths[active_synth].osc_note(i+60, note_state))
 
+#Aux button for testing
+GPIO.setup(aux_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(aux_pin, GPIO.RISING, callback = lambda pin: synths[active_synth].osc_preset('prophet'))
 while True:
     #Dial 1 amplitude
-    osc_amp(client,mcp.read_adc(0))
+    #synths[active_synth].osc_amp(mcp.read_adc(0))
     #Dial 2 pitch
-    osc_pitch(client,mcp.read_adc(3))
-    #Keyboard buttons
-    '''for i,pin in enumerate(button_pins):
-        button_state = GPIO.input(pin)
-        if button_state == False:
-            print ("Button {} pressed - Note {}".format(pin,i+60))
-            osc_note(client,i+60)'''
+    #synths[active_synth].osc_pitch(mcp.read_adc(3))
     time.sleep(0.1)
 
--- software/test.py
+++ software/test.py
@@ -1,36 +1,10 @@
-from pynput.keyboard import Key, Listener, KeyCode
-from pythonosc import udp_client
 
-
-client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
-letters =  ['a','s','d','f','g','h','j']
-letters_dict = {i:False for i in letters}
-
-
-def on_press(key):
-    key_char = str(key)[1:-1]
-    if(key_char in letters and not letters_dict[key_char]):
-        letters_dict[key_char] = True
-        print('{0} on'.format(key))
-        note = 60+letters.index(key_char)
-        client.send_message('/noteOn',note)
-        
+functions = []
+for i in range(5):
     
-def on_release(key):
-    if key == Key.esc:
-        # Stop listener
-        return False
-    
-    print('{0} off'.format(key))
-    key_char = str(key)[1:-1]
-    if(key_char in letters):
-        note = 60+letters.index(key_char)
-        client.send_message('/noteOff',note)
-        letters_dict[key_char] = False
-    
+    #GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    functions.append( lambda : print(i))
 
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+for i in range(5):
+    #GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    functions[i]()
