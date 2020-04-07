c4a0b2d757b73831613490f26ba30a973bf7a968
Carles Perez Onielfa <98perezcarles@gmail.com>
2020-04-07 16:32:52 +0200

    effects working

--- software/synthtests.py
+++ software/synthtests.py
@@ -1,16 +1,19 @@
 from pythonosc import udp_client
-
+from time import sleep
 
 client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
 
-pressed = False
+
 while(True):
     s = input("INPUT: ")
     if(s=="q"):
         break
-    if(s=="a" and not pressed):
+    if(s=="a"):
         client.send_message('/0/noteOn',60)
-        pressed = True
-    elif(s=="a" and pressed):
+        sleep(0.2)
         client.send_message('/0/noteOff',60)
-        pressed = False
+    if(s=="s"):
+        client.send_message('/1/noteOn',60)
+        sleep(0.2)
+        client.send_message('/1/noteOff',60)
+
