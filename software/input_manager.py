e9f8375df0e4671f7095e22ec8914f76a0a82c18
carlesp-onielfa <98perezcarles@gmail.com>
2020-06-28 15:21:38 +0200

    Started UI

--- software/input_manager.py
+++ software/input_manager.py
@@ -1,14 +1,15 @@
+
 from time import sleep
-from pythonosc import udp_client
-#import threading
-from multiprocessing import Process, Queue
 import RPi.GPIO as GPIO
-import OLED_Driver as OLED
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import wiringpi
+#Actions
+ACTION_VOLUME = 0
+ACTION_KEYBOARD = 1
+ACTION_BUTTON = 2
+ACTION_ENCODER = 3
 
-from PIL  import Image, ImageDraw, ImageFont, ImageColor
 class InputManager:
 
     def __init__(self):
@@ -60,9 +61,9 @@ class InputManager:
                     #print("Button pressed: {}".format(r+6*c))
                     button_n = r+6*c
                     if(button_n <= 19 and button_n >= 6):
-                        queue.put([KEYBOARD_PRESS, button_n-6])
+                        queue.put((ACTION_KEYBOARD, button_n-6))
                     else:
-                        queue.put([BUTTON_PRESS, self.buttons_order[button_n]])
+                        queue.put((ACTION_BUTTON, self.buttons_order[button_n]))
                     while(not wiringpi.digitalRead(row)):
                         pass
             wiringpi.digitalWrite(col, 1)
@@ -75,61 +76,4 @@ class InputManager:
             self.previous_pot_value = pot_value
             pot_value = round(pot_value/1023*100) 
             #JUST TO TEST
-            queue.put((VOLUME_CHANGE, pot_value))
-
-class ScreenManager:
-    def __init__(self, op_pi):
-        self.op_pi=op_pi
-        OLED.Device_Init()
-        self.test_text()
-        self.refresh_rate = 60
-    def refresh_screen(self):
-        self.test_text()
-    def test_text(self):
-        image = Image.new("RGB", (OLED.SSD1351_WIDTH, OLED.SSD1351_HEIGHT), "BLACK")
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype('fonts/ChakraPetch-Regular.ttf',18)
-
-        draw.text((0, 0), 'Volume: {}'.format(self.op_pi.volume), fill = "BLUE", font = font)
-        OLED.Display_Image(image)
-
-
-#Actions
-VOLUME_CHANGE = 0
-KEYBOARD_PRESS = 1
-BUTTON_PRESS = 2
-DIAL_CHANGE = 3
-
-class OP_Pi:
-    #Main volume
-    volume = 0
-    #State of the device 0 - main menu
-    state = 0
-    def __init__(self):
-        self.start_InputThread()
-        self.sm = ScreenManager(self)
-        self.default_loop()
-    def default_loop(self):
-        while True:
-            self.check_queue()
-            self.sm.refresh_screen()
-    def start_InputThread(self):
-        self.input_queue = Queue()
-        im = InputManager()
-        input_thread = Process(target=im.input_loop, args=(self.input_queue,), daemon=True)
-        input_thread.start()
-    def check_queue(self):
-        while(not self.input_queue.empty()):
-            action = self.input_queue.get()
-            print("OP_Pi | Action recieved: {} {}".format(action[0],action[1]))
-            if(action[0]==VOLUME_CHANGE):
-                self.volume = action[1]
-            elif(action[0]==KEYBOARD_PRESS):
-                pass
-            elif(action[0]==BUTTON_PRESS):
-                pass
-            else:
-                print("OP_Pi ERROR unrecognized action")
-
-OP_Pi()
-
+            queue.put((ACTION_VOLUME, pot_value))
