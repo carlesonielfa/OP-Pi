ff27117bbc3d6c71f4736785ed5fc4c269fbc77b
carlesp-onielfa <98perezcarles@gmail.com>
2021-02-07 19:05:57 +0100

    Text working

--- software/OP_Pi/screen_manager.cpp
+++ software/OP_Pi/screen_manager.cpp
@@ -1,12 +1,16 @@
 #include "screen_manager.h"
 
 #include <unistd.h>
+#include <cstdio>
+#include <string.h>
+
 using namespace OP_Pi;
 void ScreenManager::Draw() {
-    DrawRectangle(0,0,64,128, RED);
+    DrawText(screenWidth/2-18,18,WHITE, "128", FONT_SIZE::HUGE);
+    DrawText(screenWidth/2-8,8+16+1,WHITE, "BPM", FONT_SIZE::TINY);
+
 }
 ScreenManagerX11::ScreenManagerX11() {
-
     //Create window
     display = XOpenDisplay(0);
     int s = DefaultScreen(display);
@@ -25,6 +29,16 @@ ScreenManagerX11::ScreenManagerX11() {
         if (e.type == MapNotify)
             break;
     }
+
+    //Load fonts
+
+    char* font_names [] = {"lucidasanstypewriter-bold-8", "lucidasanstypewriter-10", "lucidasanstypewriter-12", "lucidasanstypewriter-14", "lucidasanstypewriter-18"};
+    unsigned char sizes [] = {8,10,12,14,18};
+
+    for(int i=0; i<5;i++){
+        fonts[i].font = XLoadQueryFont(display,font_names[i]);
+        fonts[i].size = sizes[i];
+    }
 }
 
 ScreenManagerX11::~ScreenManagerX11() {
@@ -40,8 +54,10 @@ void ScreenManagerX11::DrawRectangle(unsigned char x1, unsigned char y1, unsigne
     XFillRectangle(display, window,gc, x1,y1,x2-x1,y2-y1);
 }
 
-void ScreenManagerX11::DrawText() {
-
+void ScreenManagerX11::DrawText(unsigned char x, unsigned char y, unsigned long color, char* text, FONT_SIZE size) {
+    XSetFont(display,gc, fonts[size].font->fid);
+    XSetForeground(display, gc, color);
+    XDrawString(display,window, gc, x,y,text, strlen(text));
 }
 void ScreenManagerX11::Draw() {
     ScreenManager::Draw();
@@ -56,6 +72,6 @@ void ScreenManagerOLED::DrawPixel(unsigned char x, unsigned char y, unsigned lon
 
 }
 
-void ScreenManagerOLED::DrawText() {
+void ScreenManagerOLED::DrawText(unsigned char x, unsigned char y, unsigned long color, char* text, FONT_SIZE size) {
 
 }
--- software/OP_Pi/screen_manager.h
+++ software/OP_Pi/screen_manager.h
@@ -11,19 +11,34 @@ namespace OP_Pi{
     #define GRAY    0xBEBEBE
     #define CYAN    0x00FFFF
 
+    enum FONT_SIZE{
+        TINY, //8pt
+        SMALL, //10pt
+        MEDIUM, //12pt
+        BIG, //14pt
+        HUGE,  //16pt
+    };
+    /*enum FONT_ALIGN{
+        LEFT,
+        CENTER,
+        RIGHT,
+    };*/
     class ScreenManager{
     public:
         virtual void Draw();
         virtual void DrawPixel(unsigned char x, unsigned char y, unsigned long color)=0;
         virtual void DrawRectangle(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2, unsigned long color)=0;
-        virtual void DrawText()=0;
+        virtual void DrawText(unsigned char x, unsigned char y, unsigned long color, char* text, FONT_SIZE size)=0;
         void DrawMixer();
 
     protected:
         unsigned char screenHeight=128;
         unsigned char screenWidth=128;
     };
-
+    struct X11Font{
+        XFontStruct* font;
+        unsigned char size;
+    };
     class ScreenManagerX11: public ScreenManager{
     public:
         ScreenManagerX11();
@@ -31,11 +46,12 @@ namespace OP_Pi{
         void Draw() override;
         void DrawPixel(unsigned char x, unsigned char y, unsigned long color) override;
         void DrawRectangle(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2,unsigned long color) override;
-        void DrawText() override;
+        void DrawText(unsigned char x, unsigned char y, unsigned long color, char* text, FONT_SIZE size) override;
         Display* display;
     private:
         Window window;
         GC gc;
+        X11Font fonts[4];
     };
 
     class ScreenManagerOLED: public ScreenManager{
@@ -43,7 +59,7 @@ namespace OP_Pi{
         ScreenManagerOLED();
         void DrawPixel(unsigned char x, unsigned char y, unsigned long color) override;
         void DrawRectangle(unsigned char x1, unsigned char y1, unsigned char x2, unsigned char y2,unsigned long color) override;
-        void DrawText() override;
+        void DrawText(unsigned char x, unsigned char y, unsigned long color, char* text, FONT_SIZE size) override;
     };
 }
 #endif /* SCREENMANAGER_H */
