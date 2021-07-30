0ce4f69cb66bd967d9a4892d8a94bee96c835493
carlesp-onielfa <98perezcarles@gmail.com>
2021-07-30 11:47:13 +0200

    Gitignore issue fixed

--- software/OP_Pi/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/OP-Pi/software/OP_Pi
-# Build directory: /home/pi/OP-Pi/software/OP_Pi/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/libsoundio")
-subdirs("lib/lcdgfx")
